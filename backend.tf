8f0b7dc36b4a87db794815669296b33a6e416716
Akshay Telang <akshayit22@gmail.com>
2025-02-19 15:55:16 +0530

    jenkins file

---
+++ backend.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "s3" {
+    bucket         = "akshay-s3-bucket-terraform"
+    key            = "terraform_learning_multibranch/terraform.tfstate"
+    region         = "us-east-1"
+  }
+}
